9451b51ae86026d083c9d51b54285d4a23635e24
Anurag Ghosh <anurag.ghosh92@gmail.com>
2020-07-06 21:24:47 +0530

    Create SynchronizedKeyword.java
    New class demonstrating the use of synchronized keyword on a method

---
+++ src/com/concurrency/multithreading/SynchronizedKeyword.java
@@ -0,0 +1,54 @@
+package com.concurrency.multithreading;
+
+public class SynchronizedKeyword {
+	
+	private static int counter = 0;
+	
+	/*
+	 * synchronized keyword here creates a class-intrinsic lock, meaning that if a thread has acquired a lock
+	 * other threads cannot call <i>any</i> of the methods of that class.
+	 * i.e. equal to synchronized(SynchronizedKeyword.class)
+	 * */
+	private static synchronized void increment() {
+		counter++;
+	}
+	
+	public static void process() {
+		
+		Thread t1 = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				for(int i=0; i<100; i++)
+					increment();
+			}
+			
+		});
+		
+		Thread t2 = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				for(int i=0; i<100; i++)
+					increment();
+			}
+			
+		});
+		
+		t1.start();
+		t2.start();
+		
+		try {
+			t1.join();
+			t2.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		process();
+		System.out.println(counter);
+	}
+
+}
